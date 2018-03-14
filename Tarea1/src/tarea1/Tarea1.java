b35ae10c24549624904c9b7e051ddbc4ab24856a
PabloPineda <37182545+PabloPineda@users.noreply.github.com>
2018-03-14 13:32:26 -0600

    Add files via upload

---
+++ Tarea1/src/tarea1/Tarea1.java
@@ -0,0 +1,22 @@
+
+package tarea1;
+
+/**
+ *
+ * @author Pablo López
+ */
+public class Tarea1 {
+
+  
+    public static void main(String[] args) {
+        System.out.println("    -DATOS GENERALES-");
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-**-*-*-*--**-*--*");
+        System.out.println("Nombre: Pablo José.");
+        System.out.println("Apellidos: López Pineda.");
+        System.out.println("Edad: 17 anios.");
+        System.out.println("Fecha de nacimiento:19 de abril de 2000.");
+        System.out.println("Carné estudiantil: 00123617.");
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    }
+    
+}
